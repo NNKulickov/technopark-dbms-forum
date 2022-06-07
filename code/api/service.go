b762e794b98daf0ee2b3659352cd053f4ff5a4cf
nick <nkulikov@bssl.ru>
2022-06-08 01:13:46 +0300

    added clear

--- code/api/service.go
+++ code/api/service.go
@@ -1,6 +1,29 @@
 package api
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const initialScriptPath = "./DBScript/initial.sql"
 
 func GetServiceStatus(eCtx echo.Context) error { return nil }
-func ClearServiceData(eCtx echo.Context) error { return nil }
+func ClearServiceData(eCtx echo.Context) error {
+	ctx := eCtx.Request().Context()
+
+	_, err := DBS.ExecContext(ctx, `
+		truncate actor,forum,post,thread,vote
+		`)
+	sql, err := ioutil.ReadFile(initialScriptPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = DBS.ExecContext(ctx, string(sql))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return eCtx.JSON(http.StatusOK, "ok")
+}
